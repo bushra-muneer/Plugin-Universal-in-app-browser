9968fc81d4e865da2cc6a5dd9ecd86cbf20e59fd
Bushra Muneer <bushraahmed302@gmail.com>
2026-06-22 03:20:08 +0000

    style: format dart files

--- test/browser_options_test.dart
+++ test/browser_options_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:plugin_universal_in_app_browser/plugin_universal_in_app_browser.dart';
 
 void main() {
-  test('BrowserOptions toChannelPayload serializes toolbarColor and headers', () {
+  test('BrowserOptions toChannelPayload serializes toolbarColor and headers',
+      () {
     const opts = BrowserOptions(
       showTitle: false,
       toolbarColor: Color(0xFF112233),
@@ -17,6 +18,6 @@ void main() {
     expect((payload['headers'] as Map)['X-Test'], 'true');
     // toolbarColor is serialized to ARGB/int
     expect(payload['toolbarColor'], isA<int>());
-    expect(payload['toolbarColor'], equals( const Color(0xFF112233).toARGB32()));
+    expect(payload['toolbarColor'], equals(const Color(0xFF112233).toARGB32()));
   });
 }
